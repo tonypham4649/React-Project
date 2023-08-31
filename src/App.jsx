2c2f83cd6268c58800077bd97972838122cc74a4
tonypham4649 <tonypham2vn@gmail.com>
2023-08-31 17:03:18 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { Products } from "./pages/Products";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
